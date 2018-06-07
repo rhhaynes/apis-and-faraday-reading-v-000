b688ea583235ab8ae097ea8db8fd3f2539705379
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-07 13:23:10 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,7 +10,6 @@ class SearchesController < ApplicationController
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
-        req.options.timeout = 0
       end
       body = JSON.parse(@resp.body)
       @resp.success? ? (@venues = body["response"]["venues"]) : (@error = body["meta"]["errorDetail"])
